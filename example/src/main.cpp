0220dc2faf449433f38291d34cdc8b4e6cc1c29d
Z0RIK <113785827+Z0RIK@users.noreply.github.com>
2023-09-11 23:47:57 +0600

    example update

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -6,7 +6,12 @@
 
 #include "raylib.h"
 
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
+
+// User defined components
 typedef struct ParticleData {
+	size_t id;
 	Vector2 position;
 	Vector2 velocity;
 	float age;
@@ -17,12 +22,35 @@ typedef struct Sprite {
 	float radius;
 } Sprite;
 
-Scene* createScene()
+Scene* createScene(std::vector<size_t> startingAmount)
 {
-	Scene* scene = new Scene(1024);
+	size_t particleCount{};
+	std::vector<Color> colors;
+	colors.push_back(RED);
+	colors.push_back(YELLOW);
 
-	scene->createPool<ParticleData>(1024);
-	scene->createPool<Sprite>(1024);
+	for (auto it : startingAmount)
+		particleCount += it;
+
+	Scene* scene = new Scene(particleCount);
+
+	for (size_t i = 0; i < startingAmount.size(); i++)
+	{
+		for (size_t j = 0; j < startingAmount[i]; j++)
+		{
+			Entity newEntity = scene->createEntity();
+			ParticleData* data = scene->assignComponent<ParticleData>(newEntity);
+
+			data->id = i;
+			data->position = { float(rand() % GetScreenWidth()), float(rand() % GetScreenHeight()) };
+			data->velocity = { 0.0f, 0.0f };
+			//data->velocity = { float(rand() % 50 - 25), float(rand() % 50 - 25) };
+			data->age = rand() % 255;
+
+			Sprite* spr = scene->assignComponent<Sprite>(newEntity);
+			*spr = { colors[i], 3.0f};
+		}
+	}
 
 	return scene;
 }
@@ -32,15 +60,23 @@ int main()
 	float lifeTime{};
 	float spawnRate{}, spawnCounter{};
 
-	Scene* scene = createScene();
+	std::vector<size_t> startingAmount(2, 50);
+	std::vector<std::vector<float>> forceTable(2, std::vector<float>(2, 0.0f));
+
+	forceTable[0][0] = -2000.0f;
+	forceTable[0][1] = 6000.0f;
+	forceTable[1][0] = 800.0f;
+	forceTable[1][1] = 6000.0f;
 
 	InitWindow(800, 600, "[core]");
 	SetTargetFPS(60);
 
+	Scene* scene = createScene(startingAmount);
+
 	while (!WindowShouldClose())
 	{
 		// Update
-
+		/*
 		// Create new entity 
 		spawnRate = 1.0f;
 
@@ -60,24 +96,38 @@ int main()
 			*spr = { DARKGRAY, 10.0f };
 		}
 
+		*/
 		// Update velocity
 
 		for (Entity entity1 : scene->scope<ParticleData>())
 		{
-			ParticleData* data1 = scene->assignComponent<ParticleData>(entity1);
+			ParticleData* data1 = scene->getComponent<ParticleData>(entity1);
 			Vector2* pos1 = &data1->position;
+			Vector2* v1 = &data1->velocity;
+			size_t id1 = data1->id;
 
 			for (Entity entity2 : scene->scope<ParticleData>())
 			{
-				ParticleData* data2 = scene->assignComponent<ParticleData>(entity2);
+				ParticleData* data2 = scene->getComponent<ParticleData>(entity2);
 				Vector2* pos2 = &data2->position;
+				size_t id2 = data2->id;
 
-				Vector2 distanceVector = { pos2->x - pos1->x, pos2->y - pos1->x };
+				if (forceTable[id1][id2] == 0.0f) continue;
+
+				Vector2 distanceVector = { pos2->x - pos1->x, pos2->y - pos1->y };
 				float distance = sqrt(distanceVector.x * distanceVector.x + distanceVector.y * distanceVector.y);
+
+				if (distance <= 3.0f) continue;
+
+				v1->x += (distanceVector.x / (distance * distance * distance)) * forceTable[id1][id2] * GetFrameTime();
+				v1->y += (distanceVector.y / (distance * distance * distance)) * forceTable[id1][id2] * GetFrameTime();
+				//distanceVector.x /= distance;
+				//distanceVector.y /= distance;
+
 
 			}
 		}
-
+		
 		// Update position and age
 
 		for (Entity entity : scene->scope<ParticleData>())
@@ -98,8 +148,8 @@ int main()
 			// Age
 			data->age -= GetFrameTime();
 
-			if (data->age <= 0.0f) 
-				scene->destroyEntity(entity);
+			//if (data->age <= 0.0f) 
+			//	scene->destroyEntity(entity);
 			
 		}
 
